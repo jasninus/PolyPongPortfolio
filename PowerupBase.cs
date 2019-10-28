4b0d72d0dd54bc442288853c87c645f55e9ce952
jasninus <jasninus8@hotmail.dk>
2019-10-28 23:29:44 +0100

    Add relevant classes

---
+++ PowerupBase.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum PowerupTarget
+{
+    Enemies,
+    Player,
+    Ball,
+    Special
+}
+
+public enum SpawnConditions
+{
+    None,
+    NotInCircle,
+    NeedsMultipleBalls
+}
+
+public abstract class PowerupBase : MonoBehaviour
+{
+    [field: HideInInspector] public PowerupTarget Target { get; set; }
+
+    public Powerups PowerupType => powerupType;
+    [SerializeField] protected Powerups powerupType;
+
+    public SpawnConditions Conditions => spawnConditions;
+    [SerializeField] protected SpawnConditions spawnConditions;
+
+    public PowerupTarget[] ValidTargets => validTargets;
+    [SerializeField] private PowerupTarget[] validTargets;
+
+    public int SpawnLimit => spawnLimit;
+    [SerializeField] private int spawnLimit;
+
+    public float DespawnTime => despawnTime;
+    [SerializeField] private float despawnTime;
+
+    public float Duration => duration;
+    [SerializeField] private float duration;
+
+    protected virtual void EnemyActivate(List<Player> enemies)
+    {
+    }
+
+    protected virtual void PlayerActivate(Player lastPlayerHit)
+    {
+    }
+
+    protected virtual void BallActivate(GameObject ball)
+    {
+    }
+
+    protected virtual void SpecialActivate()
+    {
+    }
+
+    protected virtual void Revert()
+    {
+    }
+
+    private List<Player> GetAllEnemies(Player lastPlayerHit)
+    {
+        var enemies = new List<Player>();
+
+        foreach (var player in PlayerManager.players)
+        {
+            if (player != lastPlayerHit)
+            {
+                enemies.Add(player);
+            }
+        }
+
+        return enemies;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // If powerup collides with ball call appropriate activation method and pass relevant arguments based on powerup type
+        if (other.gameObject.tag == "Ball")
+        {
+            switch (Target)
+            {
+                case PowerupTarget.Enemies:
+                    EnemyActivate(GetAllEnemies(other.GetComponent<Ball>().lastPlayerHit));
+                    break;
+
+                case PowerupTarget.Player:
+                    if (other.GetComponent<Ball>().lastPlayerHit)
+                    {
+                        PlayerActivate(other.GetComponent<Ball>().lastPlayerHit);
+                    }
+
+                    break;
+
+                case PowerupTarget.Ball:
+                    BallActivate(other.gameObject);
+                    break;
+
+                case PowerupTarget.Special:
+                    SpecialActivate();
+                    break;
+            }
+
+            Invoke("Revert", duration);
+            Destroy(gameObject, duration + 0.1f);
+
+            // Powerup seems destroyed, but is still able to call Revert()
+            GetComponent<Collider2D>().enabled = false;
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
---
+++ SpeedPowerup.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedPowerup : PowerupBase
+{
+    [SerializeField] private float ballSpeedIncrease, playerSpeedIncrease, enemySpeedDecrease;
+
+    private Ball affectedBall;
+
+    private List<Player> affectedEnemies;
+    private Player affectedPlayer;
+
+    protected override void BallActivate(GameObject ball)
+    {
+        affectedBall = ball.GetComponent<Ball>();
+        affectedBall.ballSpeed += ballSpeedIncrease;
+    }
+
+    protected override void PlayerActivate(Player lastPlayerHit)
+    {
+        affectedPlayer = lastPlayerHit;
+        lastPlayerHit.playerSpeed += playerSpeedIncrease;
+    }
+
+    protected override void EnemyActivate(List<Player> enemies)
+    {
+        affectedEnemies = enemies;
+
+        foreach (Player enemy in enemies)
+        {
+            enemy.playerSpeed -= enemySpeedDecrease;
+        }
+    }
+
+    protected override void Revert()
+    {
+        switch (Target)
+        {
+            case PowerupTarget.Ball:
+                affectedBall.ballSpeed -= ballSpeedIncrease;
+                break;
+
+            case PowerupTarget.Player:
+                affectedPlayer.playerSpeed -= playerSpeedIncrease;
+                break;
+
+            case PowerupTarget.Enemies:
+                foreach (Player enemy in affectedEnemies)
+                {
+                    enemy.playerSpeed += enemySpeedDecrease;
+                }
+                break;
+        }
+    }
+}
